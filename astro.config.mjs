3f2c26574823b024b6b59a421fea9b1a0a763f18
sepocra <sepocra@gmail.com>
2026-02-01 20:46:26 +0800

    Change site URL and switch to Netlify adapter
    Updated site URL and changed adapter from Cloudflare to Netlify.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,10 @@
-// @ts-check
 import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify/static";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
-import cloudflare from "@astrojs/cloudflare";
-
-// https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://copescorner.sepocra.com",
   integrations: [mdx(), sitemap()],
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true,
-    },
-  }),
+  adapter: netlify()
 });
