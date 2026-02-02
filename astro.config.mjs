11a1124aaa61cc59e7f2ed1d3591eacadebfc69f
sepocra <sepocra@gmail.com>
2026-02-02 21:41:41 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from "astro/config";
-import netlify from "@astrojs/netlify";
+import netlify from "@astrojs/netlify/static";
 
 export default defineConfig({
-  output: "server",
-  adapter: netlify({
-	  edge: false
-  })
+  output: "static",
+  adapter: netlify()
 });
