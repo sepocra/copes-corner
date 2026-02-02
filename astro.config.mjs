12109ab52c85c1c903a890f0248abbbbe6390029
sepocra <sepocra@gmail.com>
2026-02-02 22:09:26 +0800

    Set default site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify/static";
 
 export default defineConfig({
+  site: process.env.SITE || "https://copescorner.sepocra.com",
   output: "static",
   adapter: netlify()
 });
