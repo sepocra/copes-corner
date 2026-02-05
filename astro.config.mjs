f037466ef133cc1392175c77c87592248ba0f33a
sepocra <sepocra@gmail.com>
2026-02-05 21:29:48 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify/static";
 
+const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
+
 export default defineConfig({
-//  site: process.env.SITE || "https://copescorner.sepocra.com",
-//  output: "static",
-//  adapter: netlify()
-site: 'https://sepocra.github.io', 
-base: '/copes-corner', 
-  output: "static" 
+  site: isGitHubPages 
+    ? 'https://sepocra.github.io' 
+    : process.env.SITE || "https://copescorner.sepocra.com",
+  base: isGitHubPages ? '/copes-corner' : undefined,
+  output: "static",
+  adapter: isGitHubPages ? undefined : netlify()
 });
