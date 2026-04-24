ef33d7671a59812fc797bedf6da7883205628305
sepocra <sepocra@gmail.com>
2026-04-24 21:49:48 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,16 @@ import mdx from "@astrojs/mdx";
 
 const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
 
+const adapter = isGitHubPages
+  ? undefined
+  : (await import("@astrojs/netlify")).default();
+
 export default defineConfig({
-  site: isGitHubPages 
-    ? 'https://sepocra.github.io' 
+  site: isGitHubPages
+    ? 'https://sepocra.github.io'
     : process.env.SITE || "https://copescorner.sepocra.com",
   base: isGitHubPages ? '/copes-corner' : undefined,
   output: "static",
   integrations: [mdx()],
-  adapter: isGitHubPages 
-    ? undefined 
-    : (await import("@astrojs/netlify/static")).default()
+  adapter,
 });
